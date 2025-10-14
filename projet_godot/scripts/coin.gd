1cd348f625ef5460a5d43da89b42fe1652a2948c
yasmine <yass.onekeo@gmail.com>
2025-10-14 14:00:19 -0400

    commit5

--- projet_godot/scripts/coin.gd
+++ projet_godot/scripts/coin.gd
@@ -1,9 +1,6 @@
 extends Area2D
 
 
-
-
-
 func _on_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
 	pass # Replace with function body.
 	queue_free()
